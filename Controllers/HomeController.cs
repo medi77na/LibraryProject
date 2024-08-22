04b846ccfa18b1a5468d5a27dd3b8a8368ee756c
medi77na <medinastevenurregodavid@gmail.com>
2024-08-22 08:29:12 -0500

    fix: se corrigen las rutas de las vistas.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,9 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpGet("Login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
 }
--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -23,6 +23,10 @@ namespace LibraryProject.Controllers
             return View();
         }
 
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
