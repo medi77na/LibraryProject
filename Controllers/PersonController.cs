870a982dfd52509754d0c50783e376cd574c41d2
medi77na <medinastevenurregodavid@gmail.com>
2024-08-22 18:36:34 -0500

    feat: se empieza con la lógica de inicio de sesión

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryProject.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using LibraryProject.Models;
+
 
 namespace LibraryProject.Controllers
 {
@@ -12,11 +15,15 @@ namespace LibraryProject.Controllers
     public class PersonController : Controller
     {
         private readonly ILogger<PersonController> _logger;
+        private readonly Bjijvrrjr8fpicdo4p4kContext _context;
 
-        public PersonController(ILogger<PersonController> logger)
+        [ActivatorUtilitiesConstructor]
+        public PersonController(ILogger<PersonController> logger, Bjijvrrjr8fpicdo4p4kContext context)
         {
+            _context = context; // Obtener la referencia al contexto de datos
             _logger = logger;
         }
+
 
         public IActionResult Index()
         {
@@ -34,7 +41,22 @@ namespace LibraryProject.Controllers
         {
             return View();
         }
-        
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Persons.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Login");
+            }
+            return View(person);
+        }
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
