475e4436aac02a21d5fcc98b7c428c470051fa10
medi77na <medinastevenurregodavid@gmail.com>
2024-08-22 11:48:24 -0500

    feat: se realiza el formulario del login

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -23,10 +23,12 @@ namespace LibraryProject.Controllers
             return View();
         }
 
-
-
-
-
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
